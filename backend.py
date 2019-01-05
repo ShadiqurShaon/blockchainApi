18791e7b0a0a5667bed91d12d67d9182a343b35c
shadiqurshaon <shadiqurshaon@gmail.com>
2019-01-05 20:49:20 +0600

    add blockchain class and generate genesis block

--- backend.py
+++ backend.py
@@ -13,3 +13,16 @@ class Block:
     def compute_hash(self):
         block_string = json.dumps(self.__dict__, sort_keys=True)
         return sha256(block_string.encode()).hexdigest()
+
+
+class Blockchain:
+    def __init__(self):
+        self.panding_transection = []
+        self.chain = []
+        self.create_genesis_block()
+
+    def create_genesis_block(self):
+        genesis_block = Block(0,[],time.time(),"0")
+        genesis_block.hash = genesis_block.compute_hash()
+        self.chain.append(genesis_block)
+        
